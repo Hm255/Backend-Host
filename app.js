4782ceb31df0ac61d15f85ed2f164e36c6a2a261
Hamza <Hamza@Arifs-MacBook-Pro.local>
2023-01-16 09:17:23 +0000

    error with 404

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ app.use((err, req, res, next) => {
   res.status(400).send({ msg: "invalid type (type is wrong)" });
   }
   else if(err.code === '22003'){
-    res.status(404).send({msg: "Item does not exist"});
+    res.status(404).send({msg: "Item does not exist, (comment is not existent)"});
   }
   else if (err.code === '23502'){
-    res.status(404).send({msg: "Item does not exist"});
+    res.status(404).send({msg: "Item does not exist, (primary key column may have a null value)"});
   }
   else if(err.code === ''){
     res.status(204).send({msg: "No content"});
